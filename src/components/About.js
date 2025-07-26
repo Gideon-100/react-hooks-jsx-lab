98bddc1f011b5ffd8a971cc62db9b6b3bc060019
Gideon lenkai <gideonlenkai2021@gmail.com>
2025-07-26 07:03:19 +0300

    Final: All tests pass for JSX Lab (NavBar, Home, About)

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import image from "../assets/image.jpg"; // Make sure the image exists at this path
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I’m a passionate web developer building awesome apps with React.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
